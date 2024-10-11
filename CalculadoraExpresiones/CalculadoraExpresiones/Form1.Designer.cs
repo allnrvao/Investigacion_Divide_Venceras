4c1416454f7b100c9bc1b20ff0f3619c255e2dd9
Armando <Avaldiviar@uamv.edu.ni>
2024-10-11 06:37:35 -0600

    Se agrega ejercicio

---
+++ CalculadoraExpresiones/CalculadoraExpresiones/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CalculadoraExpresiones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(357, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Expresiones Matematicas";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(347, 162);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 44);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(279, 89);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(234, 20);
+            this.txtExpresion.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(371, 254);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtExpresion);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ CalculadoraExpresiones/CalculadoraExpresiones/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraExpresiones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtener la expresión ingresada por el usuario
+                string expresion = txtExpresion.Text;
+
+                // Evaluar la expresión usando DataTable
+                double resultado = EvaluarExpresion(expresion);
+
+                // Mostrar el resultado en el Label
+                lblResultado.Text = "Resultado: " + resultado.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Si ocurre un error, mostrar un mensaje
+                MessageBox.Show("Error en la expresión: " + ex.Message);
+            }
+        }
+        // Método para evaluar la expresión matemática
+        private double EvaluarExpresion(string expresion)
+        {
+            // Usamos DataTable para evaluar la expresión
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expresion);
+
+            // Usamos Compute para realizar el cálculo
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+
+            // Convertimos el resultado en un valor numérico (double)
+            double resultado = double.Parse((string)row["expression"]);
+
+            return resultado;
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
